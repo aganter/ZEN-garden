dd93cdda59f2ed9869905461088a179b6a36e214
aganter <aganter@ethz.ch>
2023-11-22 16:23:53 +0100

    remove gurobi log file

--- zen_garden/model/optimization_setup.py
+++ zen_garden/model/optimization_setup.py
@@ -472,11 +472,13 @@ class OptimizationSetup(object):
         if solver_name == "gurobi_persistent":
             self.opt = pe.SolverFactory(solver_name, options=solver_options)
             self.opt.set_instance(self.model, symbolic_solver_labels=solver["use_symbolic_labels"])
-            self.results = self.opt.solve(tee=solver["verbosity"], logfile=solver["solver_options"]["logfile"], options_string=solver_parameters,save_results=False, load_solutions=False)
+            self.results = self.opt.solve(tee=solver["verbosity"], options_string=solver_parameters,save_results=False, load_solutions=False)
+            # logfile=solver["solver_options"]["logfile"]
             self.opt.load_vars()
         elif solver_name == "gurobi":
             self.opt = pe.SolverFactory(solver_name, options=solver_options)
-            self.results = self.opt.solve(self.model, tee=solver["verbosity"], keepfiles=True,logfile=solver["solver_options"]["logfile"], symbolic_solver_labels=solver["use_symbolic_labels"], options_string=solver_parameters)
+            self.results = self.opt.solve(self.model, tee=solver["verbosity"], keepfiles=True, symbolic_solver_labels=solver["use_symbolic_labels"], options_string=solver_parameters)
+            #logfile = solver["solver_options"]["logfile"],
         else:
             self.opt = pe.SolverFactory(solver_name)
             self.results = self.opt.solve(self.model, tee=solver["verbosity"], keepfiles=True, logfile=solver["solver_options"]["logfile"], symbolic_solver_labels=solver["use_symbolic_labels"])
